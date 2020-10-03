1b3c8b1d48f38734e021e10813e1b6adf1bd96e7
Gustavo Mello <gustavomelloenciv@gmail.com>
2020-10-03 15:49:21 -0300

    Etapa de construção da malha adicionada à implementação em Julia

--- otimização/algoritmos/formação_de_malha/algoritmo_de_busca.py
+++ otimização/algoritmos/formação_de_malha/algoritmo_de_busca.py
@@ -1,30 +1,20 @@
 import numba
 import numpy as np
+from collections import namedtuple
 
+Nó = namedtuple("Nó", "x y etiqueta".split())
+Elemento = namedtuple("Elemento", ["nós"])
+Malha = namedtuple("Malha", ["elementos", "nós", "me"])
 
 @numba.njit(cache=True)
 def determinar_fenótipo(gene):
     n, m = gene.shape
 
-    gene_útil = []
-    for i in range(n):
-        linha = []
-        for j in range(m):
-            linha.append(False)
-        gene_útil.append(linha)
+    fenótipo = np.full((n, m), False)
 
     # Define a posição inicial do algoritmo de busca
     i = n // 2
     j = 2*n - 1
-
-    """elementos = []
-    nós = []
-    me = []"""
-
-    # Inicializa estruturas de dados auxiliares que ajudam a manter curso dos índices dos nós e elementos
-    """etiquetas_de_elementos_já_construídos = set()
-    etiquetas_de_nós_já_construídos = set()
-    índice_na_malha = dict()"""
 
     # Inicializa parâmetros do algoritmo de busca
     possíveis_ramificações = set()
@@ -34,10 +24,6 @@ def determinar_fenótipo(gene):
     descida = True
     subida = False
 
-    # Junta as variáveis importantes para métodos auxiliares
-    """contexto = (l, gene_útil, elementos, nós, me, índice_na_malha,
-                etiquetas_de_nós_já_construídos, etiquetas_de_elementos_já_construídos)
-    """
     # Executa o algoritmo de busca
     while buscando:
 
@@ -49,26 +35,25 @@ def determinar_fenótipo(gene):
 
             # consigo descer mais?
             if i != n - 1:
-                abaixo = gene[i + 1][j]
+                abaixo = gene[i + 1, j]
                 descida = abaixo
             else:
                 descida = False
 
             # há ramificações possíveis aqui do lado?
             if j != 2 * n - 1 and último_movimento != "esquerda":
-                direita = gene[i][j + 1]
-                if direita and not gene_útil[i][j + 1]:
+                direita = gene[i, j + 1]
+                if direita and not fenótipo[i, j + 1]:
                     possíveis_ramificações.add((i, j + 1, "direita"))
 
             if j == 0:
                 borda_alcançada = True
             elif último_movimento != "direita":
-                esquerda = gene[i][j - 1]
-                if esquerda and not gene_útil[i][j - 1]:
+                esquerda = gene[i, j - 1]
+                if esquerda and not fenótipo[i, j - 1]:
                     possíveis_ramificações.add((i, j - 1, "esquerda"))
 
-            gene_útil[i][j] = True
-            # adicionar_à_malha_o_elemento_em(i, j, contexto=contexto)
+            fenótipo[i, j] = True
             possíveis_ramificações.discard((i, j, "esquerda"))
             possíveis_ramificações.discard((i, j, "direita"))
 
@@ -78,7 +63,7 @@ def determinar_fenótipo(gene):
                 último_movimento = "baixo"
             else:
                 if partida[0] != 0:
-                    subida = gene[partida[0] - 1][partida[1]]
+                    subida = gene[partida[0] - 1, partida[1]]
                 else:
                     subida = False
 
@@ -90,27 +75,26 @@ def determinar_fenótipo(gene):
 
             # consigo subir mais?
             if i != 0:
-                acima = gene[i - 1][j]
+                acima = gene[i - 1, j]
                 subida = acima
             else:
                 subida = False
 
             # há ramificações possíveis aqui do lado?
             if j != 2 * n - 1 and último_movimento != "esquerda":
-                direita = gene[i][j + 1]
-                if direita and not gene_útil[i][j + 1]:
+                direita = gene[i, j + 1]
+                if direita and not fenótipo[i, j + 1]:
                     possíveis_ramificações.add((i, j + 1, "direita"))
 
             if j == 0:
                 borda_alcançada = True
 
             if j != 0 and último_movimento != "direita":
-                esquerda = gene[i][j - 1]
-                if esquerda and not gene_útil[i][j - 1]:
+                esquerda = gene[i, j - 1]
+                if esquerda and not fenótipo[i, j - 1]:
                     possíveis_ramificações.add((i, j - 1, "esquerda"))
 
-            gene_útil[i][j] = True
-            # adicionar_à_malha_o_elemento_em(i, j, contexto=contexto)
+            fenótipo[i, j] = True
             possíveis_ramificações.discard((i, j, "esquerda"))
             possíveis_ramificações.discard((i, j, "direita"))
 
@@ -127,13 +111,177 @@ def determinar_fenótipo(gene):
         else:
             buscando = False
 
-    # Transforma a lista de tuplas em matriz
-    # me = inicializar_matriz_(me)
-
-    return gene_útil, borda_alcançada #, elementos, nós, me
+    return fenótipo, borda_alcançada
 
 
-def teste_determinar_fenótipo():
+def determinar_fenótipo_sem_compilar(gene):
+    def adicionar_à_malha_o_elemento_em(i: int, j: int) -> None:
+        # Inicializa os nós dos cantos do elemento
+        y = 1 - i*l
+        ul, ur, dr, dl = Nó(      j*l,     y, etiqueta=(    i,     j)), \
+                         Nó((j + 1)*l,     y, etiqueta=(    i, j + 1)), \
+                         Nó((j + 1)*l, y - l, etiqueta=(i + 1, j + 1)), \
+                         Nó(      j*l, y - l, etiqueta=(i + 1,     j))
+
+        índices_globais_dos_cantos = []
+
+        # Para cada nó em cada canto
+        for nó in (ul, ur, dr, dl):
+
+            # Se o índice do nó ainda não foi visto
+            if nó.etiqueta not in etiquetas_de_nós_já_construídos:
+
+                # Adiciona o canto à lista de nós que comporão a malha
+                nós.append(nó)
+
+                # Define o índice do canto na malha como o índice corrente
+                índice_na_malha[nó.etiqueta] = len(etiquetas_de_nós_já_construídos)
+                índices_globais_dos_cantos.append(len(etiquetas_de_nós_já_construídos))
+
+                # Define que o nó já foi visto
+                etiquetas_de_nós_já_construídos.add(nó.etiqueta)
+
+            else:
+                índices_globais_dos_cantos.append(índice_na_malha[nó.etiqueta])
+
+        iul, iur, idr, idl = índices_globais_dos_cantos
+
+        # Atualiza a matriz de correspondência entre os índices
+        # locais e globais dos nós para o elemento atual
+        me.append((2 * iul,
+                   2 * iul + 1,
+                   2 * iur,
+                   2 * iur + 1,
+                   2 * idr,
+                   2 * idr + 1,
+                   2 * idl,
+                   2 * idl + 1))
+
+        # Cria um elemento com os cantos e o adiciona
+        # à lista daqueles que comporão a malha
+        if ul.etiqueta not in etiquetas_de_elementos_já_construídos:
+            elementos.append(Elemento((ul, ur, dr, dl)))
+            etiquetas_de_elementos_já_construídos.add(ul.etiqueta)
+
+    n, m = gene.shape
+    l = 1/n
+
+    fenótipo = np.zeros((n, m), dtype=bool)
+
+    # Define a posição inicial do algoritmo de busca
+    i = n // 2
+    j = 2*n - 1
+
+    elementos = []
+    nós = []
+    me = []
+
+    # Inicializa estruturas de dados auxiliares que ajudam a manter curso dos índices dos nós e elementos
+    etiquetas_de_elementos_já_construídos = set()
+    etiquetas_de_nós_já_construídos = set()
+    índice_na_malha = dict()
+
+    # Inicializa parâmetros do algoritmo de busca
+    possíveis_ramificações = set()
+    último_movimento = "esquerda"
+    borda_alcançada = False
+    buscando = True
+    descida = True
+    subida = False
+
+    # Executa o algoritmo de busca
+    while buscando:
+
+        # busca vertical
+        partida = (i, j)
+
+        # começar descida
+        while descida:
+
+            # consigo descer mais?
+            if i != n - 1:
+                abaixo = gene[i + 1, j]
+                descida = abaixo
+            else:
+                descida = False
+
+            # há ramificações possíveis aqui do lado?
+            if j != 2 * n - 1 and último_movimento != "esquerda":
+                direita = gene[i, j + 1]
+                if direita and not fenótipo[i, j + 1]:
+                    possíveis_ramificações.add((i, j + 1, "direita"))
+
+            if j == 0:
+                borda_alcançada = True
+            elif último_movimento != "direita":
+                esquerda = gene[i, j - 1]
+                if esquerda and not fenótipo[i, j - 1]:
+                    possíveis_ramificações.add((i, j - 1, "esquerda"))
+
+            fenótipo[i, j] = True
+            adicionar_à_malha_o_elemento_em(i, j)
+            possíveis_ramificações.discard((i, j, "esquerda"))
+            possíveis_ramificações.discard((i, j, "direita"))
+
+            # Decide se continua descendo ou se passa a subir
+            if descida:
+                i = i + 1
+                último_movimento = "baixo"
+            else:
+                if partida[0] != 0:
+                    subida = gene[partida[0] - 1, partida[1]]
+                else:
+                    subida = False
+
+                if subida:
+                    i = partida[0] - 1
+
+        # começar subida
+        while subida:
+
+            # consigo subir mais?
+            if i != 0:
+                acima = gene[i - 1, j]
+                subida = acima
+            else:
+                subida = False
+
+            # há ramificações possíveis aqui do lado?
+            if j != 2 * n - 1 and último_movimento != "esquerda":
+                direita = gene[i, j + 1]
+                if direita and not fenótipo[i, j + 1]:
+                    possíveis_ramificações.add((i, j + 1, "direita"))
+
+            if j == 0:
+                borda_alcançada = True
+
+            if j != 0 and último_movimento != "direita":
+                esquerda = gene[i, j - 1]
+                if esquerda and not fenótipo[i, j - 1]:
+                    possíveis_ramificações.add((i, j - 1, "esquerda"))
+
+            fenótipo[i, j] = True
+            adicionar_à_malha_o_elemento_em(i, j)
+            possíveis_ramificações.discard((i, j, "esquerda"))
+            possíveis_ramificações.discard((i, j, "direita"))
+
+            # Decide se continua descendo ou se passa a subir
+            if subida:
+                i = i - 1
+                último_movimento = "cima"
+
+        if len(possíveis_ramificações) > 0:
+            i, j, último_movimento = possíveis_ramificações.pop()
+            descida = True
+            subida = False
+
+        else:
+            buscando = False
+
+    return fenótipo, borda_alcançada, elementos, nós, me
+
+
+def teste_determinar_fenótipo(compilar=True, checar=False):
     T = True
     f = False
         
@@ -143,12 +291,25 @@ def teste_determinar_fenótipo():
                            [T, f, f, f, f, T, T, T, T, f],
                            [T, T, f, f, f, f, T, f, f, f]], dtype=bool)
     
-    resultado_esperado = np.array([[T, T, f, f, T, T, f, f, f, f],
-                                   [f, T, f, f, T, f, f, f, f, f],
-                                   [f, T, T, T, T, T, f, f, T, T],
-                                   [f, f, f, f, f, T, T, T, T, f],
-                                   [f, f, f, f, f, f, T, f, f, f]], dtype=bool)
-    
-    resultado, borda_alcançada = determinar_fenótipo(gene_teste)
-    assert np.all(resultado == resultado_esperado)
-    assert borda_alcançada
+    if compilar:
+        resultado, borda_alcançada = determinar_fenótipo(gene_teste)
+    else:
+        resultado, borda_alcançada, elementos, nós, me = determinar_fenótipo_sem_compilar(gene_teste)
+
+    if checar:
+        resultado_esperado = np.array([[T, T, f, f, T, T, f, f, f, f],
+                                       [f, T, f, f, T, f, f, f, f, f],
+                                       [f, T, T, T, T, T, f, f, T, T],
+                                       [f, f, f, f, f, T, T, T, T, f],
+                                       [f, f, f, f, f, f, T, f, f, f]], dtype=bool)
+        assert np.all(resultado == resultado_esperado)
+        assert borda_alcançada
+
+        if not compilar:
+            assert len(elementos) == resultado_esperado.sum()
+            assert len(nós) == 38
+
+            malha = Malha(elementos, nós, me)
+            for at in malha:
+                print(at)
+                print("\n")
--- otimização/algoritmos/formação_de_malha/julia_alg_busca_e_construção.jl
+++ otimização/algoritmos/formação_de_malha/julia_alg_busca_e_construção.jl
@@ -1,141 +1,237 @@
 module AlgBC
 
+import Base.show
+import Base.hash
+
+using Printf
+
 export teste_determinar_fenótipo
 
-    function determinar_fenótipo(gene)
-        fenótipo = zeros(Bool, size(gene))
-        n, _ = size(fenótipo)
+struct Nó
+    x::Float64
+    y::Float64
+    etiqueta::NTuple{2, Int}
+end
+    hash(nó::Nó) = hash(nó.etiqueta)
+    function show(io::IO, nó::Nó) 
+        print(io, "Nó(x=$(@sprintf("%.2f", nó.x)), y=$(@sprintf("%.2f", nó.y)), etiqueta=$(nó.etiqueta))")
+    end
 
-        i = n÷2 + 1
-        j = 2n
 
-        possíves_ramificações = Set()
-        último_movimento = "esquerda"
-        borda_alcançada = false
-        buscando = true
-        descida = true
-        subida = false
+struct Elemento
+    nós::Tuple{Vararg{Nó}}
+end
 
-        while buscando
-            partida = (i, j)
 
-            while descida
-                if i ≠ n
-                    abaixo = gene[i + 1, j]
-                    descida = abaixo                
-                else
-                    descida = false
+struct Malha
+    elementos::Tuple{Vararg{Elemento}}
+    nós::Tuple{Vararg{Nó}}
+    me::Matrix{UInt}
+    ne::Int
+    índice_de::Dict{Nó, Int}
+
+    function Malha(elementos, nós, me)
+        ne = length(elementos)
+        índice_de = Dict(nó => i for (i, nó) in enumerate(nós) )
+        return new(elementos, nós, me, ne, índice_de)
+    end
+end
+    function show(m::Malha)
+        saída = "["
+        for elem in m.elementos
+            nó = elem.nós[1]
+            saída *= "Elemento com UL: $(nó.etiqueta), "
+        end
+        print(saída[1:end - 2] * "]")
+    end
+
+
+function determinar_fenótipo(gene)
+    function adicionar_à_malha_o_elemento_em_(i, j)
+        fenótipo[i, j] = true
+        
+        x = (j - 1)*l
+        y = 1 - (i - 1)*l
+        (ul, ur, dr, dl) = (Nó(    x,     y, (    i,     j)),
+                            Nó(x + l,     y, (    i, j + 1)),
+                            Nó(x + l, y - l, (i + 1, j + 1)),
+                            Nó(    x, y - l, (i + 1,     j)))
+
+        índices_globais_dos_cantos = []
+
+        for nó ∈ (ul, ur, dr, dl) 
+            if nó.etiqueta ∉ etiquetas_de_nós_já_construídos
+                push!(nós, nó)
+
+                índice_na_malha[nó.etiqueta] = length(etiquetas_de_nós_já_construídos)
+                push!(índices_globais_dos_cantos, length(etiquetas_de_nós_já_construídos))
+
+                push!(etiquetas_de_nós_já_construídos, nó.etiqueta)
+            else
+                push!(índices_globais_dos_cantos, índice_na_malha[nó.etiqueta])
+            end
+        end
+
+        iul, iur, idr, idl = índices_globais_dos_cantos
+        
+        me = [me [2iul, 2iul + 1, 2iur, 2iur + 1, 2idr, 2idr + 1, 2idl, 2idl + 1]]
+
+        if ul.etiqueta ∉ etiquetas_de_elementos_já_construídos
+            push!(elementos, Elemento((ul, ur, dr, dl)))
+            push!(etiquetas_de_elementos_já_construídos, ul.etiqueta)
+        end
+
+    end #function adicionar_à_malha_o_elemento_em_
+
+    elementos = []
+    nós = []
+    me = Matrix{Union{UInt, Missing}}(missing, 8, 0)
+
+    etiquetas_de_elementos_já_construídos = Set()
+    etiquetas_de_nós_já_construídos = Set()
+    índice_na_malha = Dict()
+
+    fenótipo = zeros(Bool, size(gene))
+    n, m = size(fenótipo)
+
+    i = n÷2 + 1
+    j = m
+
+    l = 1/n
+
+    possíves_ramificações = Set()
+    último_movimento = "esquerda"
+    borda_alcançada = false
+    buscando = true
+    descida = true
+    subida = false
+
+    while buscando
+        partida = (i, j)
+
+        while descida
+            if i ≠ n
+                abaixo = gene[i + 1, j]
+                descida = abaixo                
+            else
+                descida = false
+            end
+
+            if (j ≠ 2n) & (último_movimento ≠ "esquerda")
+                direita = gene[i, j + 1]
+                if direita & ~fenótipo[i, j + 1]
+                    push!(possíves_ramificações, (i, j + 1, "direita"))
                 end
+            end
 
-                if (j ≠ 2n) & (último_movimento ≠ "esquerda")
-                    direita = gene[i, j + 1]
-                    if direita & ~fenótipo[i, j + 1]
-                        push!(possíves_ramificações, (i, j + 1, "direita"))
-                    end
+            if j ≡ 1
+                borda_alcançada = true
+            elseif último_movimento ≠ "direita"
+                esquerda = gene[i, j - 1]
+                if esquerda & ~fenótipo[i, j - 1]
+                    push!(possíves_ramificações, (i, j - 1, "esquerda"))
                 end
+            end
 
-                if j ≡ 1
-                    borda_alcançada = true
-                elseif último_movimento ≠ "direita"
-                    esquerda = gene[i, j - 1]
-                    if esquerda & ~fenótipo[i, j - 1]
-                        push!(possíves_ramificações, (i, j - 1, "esquerda"))
-                    end
-                end
+            fenótipo[i, j] = true
+            setdiff!(possíves_ramificações, ((i, j, "esquerda"), (i, j, "direita")))
+            adicionar_à_malha_o_elemento_em_(i, j)
 
-                fenótipo[i, j] = true
-
-                # Aqui entra a função de adicionar o elemento à malha
-                setdiff!(possíves_ramificações, ((i, j, "esquerda"), (i, j, "direita")))
-
-                if descida
-                    i += 1
-                    último_movimento = "baixo"
-                else
-                    i_p, j_p = partida
-                    if i_p ≠ 1
-                        subida = gene[i_p - 1, j_p]
-                    else
-                        subida = false
-                    end
-
-                    if subida
-                        i = i_p - 1
-                    end
-                end
-            end #descida
-
-            while subida
-                
-                if i ≠ 1
-                    acima = gene[i - 1, j]
-                    subida = acima
+            if descida
+                i += 1
+                último_movimento = "baixo"
+            else
+                i_p, j_p = partida
+                if i_p ≠ 1
+                    subida = gene[i_p - 1, j_p]
                 else
                     subida = false
                 end
 
-                if (j ≠ 2n) & (último_movimento ≠ "esquerda")
-                    direita = gene[i, j + 1]
-                    if direita & ~fenótipo[i, j + 1]
-                        push!(possíves_ramificações, (i, j + 1, "direita"))
-                    end
-                end
-
-                if j ≡ 1
-                    borda_alcançada = true
-                elseif último_movimento ≠ "direita"
-                    esquerda = gene[i, j - 1]
-                    if esquerda & ~fenótipo[i, j - 1]
-                        push!(possíves_ramificações, (i, j - 1, "esquerda"))
-                    end
-                end
-
-                fenótipo[i, j] = true
-
-                # Aqui entra a função de adicionar o elemento à malha
-                setdiff!(possíves_ramificações, ((i, j, "esquerda"), (i, j, "direita")))
-
                 if subida
-                    i -= 1
-                    último_movimento = "cima"
+                    i = i_p - 1
                 end
+            end
+        end #descida
 
-            end #subida
-        
-            if length(possíves_ramificações) > 0
-                i, j, último_movimento = pop!(possíves_ramificações)
-                descida = true
-                subida = false
+        while subida
+            
+            if i ≠ 1
+                acima = gene[i - 1, j]
+                subida = acima
             else
-                buscando = false
+                subida = false
             end
 
-        end #busca
+            if (j ≠ 2n) & (último_movimento ≠ "esquerda")
+                direita = gene[i, j + 1]
+                if direita & ~fenótipo[i, j + 1]
+                    push!(possíves_ramificações, (i, j + 1, "direita"))
+                end
+            end
 
-        return fenótipo, borda_alcançada
-    end #determinar_fenótipo
+            if j ≡ 1
+                borda_alcançada = true
+            elseif último_movimento ≠ "direita"
+                esquerda = gene[i, j - 1]
+                if esquerda & ~fenótipo[i, j - 1]
+                    push!(possíves_ramificações, (i, j - 1, "esquerda"))
+                end
+            end
+
+            fenótipo[i, j] = true
+            setdiff!(possíves_ramificações, ((i, j, "esquerda"), (i, j, "direita")))
+            adicionar_à_malha_o_elemento_em_(i, j)
+
+            if subida
+                i -= 1
+                último_movimento = "cima"
+            end
+
+        end #subida
+    
+        if length(possíves_ramificações) > 0
+            i, j, último_movimento = pop!(possíves_ramificações)
+            descida = true
+            subida = false
+        else
+            buscando = false
+        end
+
+    end #busca
+
+    return fenótipo, borda_alcançada, Tuple(elementos), Tuple(nós), me
+end #determinar_fenótipo
 
 
-    function teste_determinar_fenótipo()
-        T = true
-        f = false
-        
-        gene_teste = [[T T f f T T f f f f];
-                      [f T f f T f f f f f];
-                      [f T T T T T f f T T];
-                      [T f f f f T T T T f];
-                      [T T f f f f T f f f]]
-        
+function teste_determinar_fenótipo(checar=false)
+    T = true
+    f = false
+    
+    gene_teste = [[T T f f T T f f f f];
+                  [f T f f T f f f f f];
+                  [f T T T T T f f T T];
+                  [T f f f f T T T T f];
+                  [T T f f f f T f f f]]
+    
+    resultado, borda_alcançada, elementos, nós, me = determinar_fenótipo(gene_teste)
+
+    if checar
         resultado_esperado = [[T T f f T T f f f f];
                               [f T f f T f f f f f];
                               [f T T T T T f f T T];
                               [f f f f f T T T T f];
                               [f f f f f f T f f f]]
         
-        
-        resultado, borda_alcançada = determinar_fenótipo(gene_teste)
-        @assert assertion_1 = all(resultado .≡ resultado_esperado)
-        @assert assertion_2 = borda_alcançada 
-    end #teste_determinar_fenótipo
+        @assert borda_alcançada
+        @assert all(resultado .≡ resultado_esperado)
+        @assert length(elementos) ≡ sum(resultado_esperado)
+        @assert length(nós) ≡ 38
+
+        show(Malha(elementos, nós, me))
+        println("\n")
+        show(nós)
+    end
+end #teste_determinar_fenótipo
 
 end #AlgBC
